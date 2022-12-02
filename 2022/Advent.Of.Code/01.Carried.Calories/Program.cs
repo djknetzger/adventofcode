97af0dcbc23a117fc3f58755344d7b38362c6ac4
DJ Knetzger <dean.knetzger@gmail.com>
2022-12-01 23:04:17 -0600

    Day 1 complete

---
+++ 2022/Advent.Of.Code/01.Carried.Calories/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _01.Carried.Calories
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"input.txt");
+
+            List<int> totalCals = new List<int>();
+
+            var batchTotal = 0;
+            foreach (string line in lines)
+            {
+
+                if (!string.IsNullOrEmpty(line))
+                {
+                    batchTotal += int.Parse(line);
+                }
+                else
+                {
+                    totalCals.Add(batchTotal);
+                    batchTotal = 0;
+                }
+            }
+
+            Console.WriteLine($"Max Calories: {totalCals.Max()}");
+
+            totalCals.Sort();
+
+            var topThreeCals = totalCals.TakeLast(3).Sum();
+
+            Console.WriteLine($"Sum of top 3 carriers: {topThreeCals}");
+        }
+    }
+}
